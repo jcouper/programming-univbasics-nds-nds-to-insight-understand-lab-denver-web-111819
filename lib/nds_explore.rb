bad64ff9447a9a27d0683cb1741009273285a074
Josh Couper <joshcouper@gmail.com>
2020-02-04 17:53:45 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,14 +13,11 @@ end
 
 
 def print_first_directors_movie_titles
+  def new_line(var)
+    /n 
   
-  # Array
-  #1st item is a hash with a key of dirname 
-  #the name is stephen spelkjaf
-  # the second key in the first hash is movies 
-  #movies is a hash 
-  # 
-  
+  end
+
   
   directors_database[0][:name][:movies][0][:title]
   
